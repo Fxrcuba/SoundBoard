7aac21d4cd11ef9140833165b28a2b4b61cc156c
Maria Fernanda Cuba Moriano <maria.cuba@tecsup.edu.pe>
2024-10-07 17:09:42 -0500

    Initial Commit

---
+++ CubaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CubaSoundBoard
+//
+//  Created by Fernanda Cuba on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
